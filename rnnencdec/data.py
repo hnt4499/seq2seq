ab39495c2ffb70e7c336d885e76a07eb797768f0
hnt4499 <hnt4499@gmail.com>
2020-12-13 00:28:50 +0800

    Add data utilities

---
+++ rnnencdec/data.py
@@ -0,0 +1,84 @@
+from random import shuffle
+
+import torch
+import pandas as pd
+from torch.utils.data import IterableDataset
+
+
+class CustomDataset(IterableDataset):
+    """Custom dataset that reads one bitext chunk at a time.
+    
+    Parameters
+    ----------
+    bitext_file : str
+        Path to the bitext file (*.csv) with two columns "source" and "target".
+    src_vocab : str
+        A dictionary obtained from the script `scripts/build_truncated_vocab.py` containing
+        two keys: "tok2idx" and "idx2tok".
+    tgt_vocab : str
+        A dictionary obtained from the script `scripts/build_truncated_vocab.py` containing
+        two keys: "tok2idx" and "idx2tok".
+    num_samples : int
+        Number of samples of this particular bitext chunk.
+    shuffle : bool
+        Whether to shuffle the dataset.
+    """
+    def __init__(self, bitext_file, src_vocab, tgt_vocab, num_samples=None, shuffle=True):
+        super(CustomDataset, self).__init__()
+        self.bitext_file = bitext_file
+        self.src_vocab = src_vocab
+        self.tgt_vocab = tgt_vocab
+        self.shuffle = shuffle
+
+        if num_samples is None:
+            self.bitext = pd.read_csv(self.bitext_file)
+            self.num_samples = len(self.bitext)
+        else:
+            self.bitext = None  # lazy init
+            self.num_samples = num_samples
+        # For random indexing
+        self.idxs = list(range(self.num_samples))
+        self.curr_idx = 0
+
+    def __len__(self):
+        return self.num_samples
+        
+
+    def __iter__(self):
+        if self.bitext is None:
+            self.bitext = pd.read_csv(self.bitext_file)
+        # Intialize
+        if self.shuffle:
+            shuffle(self.idxs)
+        self.curr_idx = 0
+
+        return self
+
+    def __next__(self):
+        # Stop if needed
+        if self.curr_idx >= self.num_samples:
+            self.bitext = None  # free memory
+            raise StopIteration
+
+        idx = self.idxs[self.curr_idx]
+        src_text, tgt_text = self.bitext.loc[idx, ["source", "target"]]
+        # Tokenize and convert to index
+        src_tok2idx = self.src_vocab["tok2idx"]
+        src_sos = src_tok2idx["<sos>"]
+        src_eos = src_tok2idx["<eos>"]
+
+        tgt_tok2idx = self.tgt_vocab["tok2idx"]
+        tgt_sos = tgt_tok2idx["<sos>"]
+        tgt_eos = tgt_tok2idx["<eos>"]
+
+        src_toks = [src_tok2idx.get(tok, src_tok2idx["<unk>"]) 
+                    for tok in src_text.split()]
+        src_toks = [src_sos] + src_toks + [src_eos]
+
+        tgt_toks = [tgt_tok2idx.get(tok, tgt_tok2idx["<unk>"]) 
+                    for tok in tgt_text.split()]
+        tgt_toks = [tgt_sos] + tgt_toks + [tgt_eos]
+
+        # Update index
+        self.curr_idx += 1
+        return torch.tensor(src_toks, dtype=torch.int16), torch.tensor(tgt_toks, dtype=torch.int16)
---
+++ scripts/build_truncated_vocab.py
@@ -0,0 +1,44 @@
+DESCRIPTION = """
+Given the full vocab obtained from `build_vocab.py`, build the truncated vocab
+into a json file with tok2idx and idx2tok fields.
+"""
+
+import sys
+import json
+import argparse
+
+import pandas as pd
+
+
+SPECIAL_TOKENS = ["<pad>", "<unk>", "<sos>", "<eos>"]
+
+
+def main(args):
+    args = vars(args)
+    # Read full vocab
+    full_vocab = pd.read_csv(args["full_vocab_path"])
+    full_vocab = full_vocab.sort_values("count", ascending=False)
+    # Process
+    tokens = SPECIAL_TOKENS + full_vocab["word"].iloc[:args["vocab_size"]].to_list()
+    tok2idx = dict(zip(tokens, range(len(tokens))))
+    idx2tok = dict(zip(range(len(tokens)), tokens))
+    # Save truncated vocab
+    with open(args["save_path"], "w") as fout:
+        json.dump({"tok2idx": tok2idx, "idx2tok": idx2tok}, fout)
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=DESCRIPTION)
+    parser.add_argument('-f', '--full-vocab-path', type=str, required=True,
+        help='Path to the full vocab obtained from `build_vocab.py`.')
+    parser.add_argument('-s', '--save-path', type=str, required=True,
+        help='Path to save the truncated vocab (*.json).')
+    parser.add_argument('--vocab-size', type=int, required=True,
+        help='Vocab size (not counting special tokens).')
+
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
