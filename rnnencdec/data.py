7abaf8c0aea95da2ad67cdca76f1100083d97f56
hnt4499 <hnt4499@gmail.com>
2020-12-13 16:07:11 +0800

    Allow one bitext file at a time (to avoid memory issues)

--- rnnencdec/data.py
+++ rnnencdec/data.py
@@ -10,8 +10,8 @@ class CustomDataset(IterableDataset):
     
     Parameters
     ----------
-    bitext_files : str or list of str
-        Path to the bitext file(s) (*.csv) with two columns "source" and "target".
+    bitext_file : str or list of str
+        Path to the bitext file (*.csv) with two columns "source" and "target".
     src_vocab : str
         A dictionary obtained from the script `scripts/build_truncated_vocab.py` containing
         two keys: "tok2idx" and "idx2tok".
@@ -23,20 +23,17 @@ class CustomDataset(IterableDataset):
     shuffle : bool
         Whether to shuffle the dataset.
     """
-    def __init__(self, bitext_files, src_vocab, tgt_vocab, num_samples=None, shuffle=True):
+    def __init__(self, bitext_file, src_vocab, tgt_vocab, num_samples=None, shuffle=True):
         super(CustomDataset, self).__init__()
-        self.bitext_files = bitext_files
+        self.bitext_file = bitext_file
         self.src_vocab = src_vocab
         self.tgt_vocab = tgt_vocab
         self.shuffle = shuffle
 
-        if isinstance(self.bitext_files, str):
-            self.bitext_files = [self.bitext_files]
-
         if num_samples is None:
-            self.bitext = [pd.read_csv(bitext_file) for bitext_file in self.bitext_files]
-            self.bitext = pd.concat(self.bitext, axis=1)
+            self.bitext = pd.read_csv(bitext_file)
             self.num_samples = len(self.bitext)
+            self.bitext = None  # free memory
         else:
             self.bitext = None  # lazy init
             self.num_samples = num_samples
@@ -50,8 +47,7 @@ class CustomDataset(IterableDataset):
 
     def __iter__(self):
         if self.bitext is None:
-            self.bitext = [pd.read_csv(bitext_file) for bitext_file in self.bitext_files]
-            self.bitext = pd.concat(self.bitext, axis=1)
+            self.bitext = pd.read_csv(self.bitext_file)
         # Intialize
         if self.shuffle:
             shuffle(self.idxs)
