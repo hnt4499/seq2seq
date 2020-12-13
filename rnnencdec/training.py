7a032a3671f122a7ac342c2cd6b6cdd7eea9e438
hnt4499 <hnt4499@gmail.com>
2020-12-14 04:03:10 +0800

    Add more info to the progress bar

--- rnnencdec/training.py
+++ rnnencdec/training.py
@@ -11,7 +11,8 @@ from tqdm import tqdm
 import torch
 
 
-def train(model, dataloader, optimizer, criterion, device, gradient_clip=None):
+def train(model, dataloader, optimizer, criterion, device, gradient_clip=None,
+          epoch=None, total_epoch=None):
     """Train the model.
 
     Parameters
@@ -29,12 +30,21 @@ def train(model, dataloader, optimizer, criterion, device, gradient_clip=None):
     gradient_clip : float
         Value to clip the gradient. If None, gradient clipping will not be
         performed.
+    epoch : int
+        Current epoch index. Used to print meaningful progress bar with tqdm if
+        specified.
+    total_epoch : int
+        Total number of epochs. Used to print meaningful progress bar with tqdm
+        if specified.
     """
     model.train()
     epoch_loss = 0
 
     with tqdm(dataloader) as t:
-        t.set_description("Training")
+        if epoch is not None and total_epoch is not None:
+            t.set_description(f"Training ({epoch}/{total_epoch})")
+        else:
+            t.set_description("Training")
         for i, (src, tgt) in enumerate(t):
             src, tgt = src.to(device), tgt.to(device)
             optimizer.zero_grad()
@@ -61,7 +71,8 @@ def train(model, dataloader, optimizer, criterion, device, gradient_clip=None):
     return epoch_loss / (len(dataloader) + 1)
 
 
-def evaluate(model, dataloader, criterion, device):
+def evaluate(model, dataloader, criterion, device, epoch=None,
+             total_epoch=None):
     """Evaluate the trained model.
 
     Parameters
@@ -74,13 +85,22 @@ def evaluate(model, dataloader, criterion, device):
         Loss (objective) function.
     device : torch.device
         Device in which computation is performed.
+    epoch : int
+        Current epoch index. Used to print meaningful progress bar with tqdm if
+        specified.
+    total_epoch : int
+        Total number of epochs. Used to print meaningful progress bar with tqdm
+        if specified.
     """
     model.eval()
     epoch_loss = 0
 
     with torch.no_grad():
         with tqdm(dataloader) as t:
-            t.set_description("Evaluating:")
+            if epoch is not None and total_epoch is not None:
+                t.set_description(f"Evaluating ({epoch}/{total_epoch})")
+            else:
+                t.set_description("Evaluating")
             for i, (src, tgt) in enumerate(t):
                 src, tgt = src.to(device), tgt.to(device)
 
